4ac226a9ba0e548b2fa95f199520d61de7381779
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2021-06-14 16:00:47 +0700

    update

--- src/components/smooth.jsx
+++ src/components/smooth.jsx
@@ -1,55 +1,60 @@
 const SmoothScroll = (target, speed, smooth) => {
   if (target === document)
-    target = (document.documentElement || document.body.parentNode || document.body) // cross browser support for document scrolling
+    target =
+      document.documentElement || document.body.parentNode || document.body; // cross browser support for document scrolling
 
   let moving = false;
   let pos = target.scrollTop;
-  target.addEventListener('mousewheel', scrolled, { passive: false })
+  target.addEventListener("mousewheel", scrolled, { passive: false });
   /* target.addEventListener('DOMMouseScroll', scrolled, false) */
 
   function scrolled(e) {
+    if (!moving) {
+      pos = target.scrollTop;
+    }
     e.preventDefault(); // disable default scrolling
     let delta = e.delta || e.wheelDelta;
     if (delta === undefined) {
       //we are on firefox
       delta = -e.detail;
     }
-    delta = Math.max(-1, Math.min(1, delta)) // cap the delta to [-1,1] for cross browser consistency
+    delta = Math.max(-1, Math.min(1, delta)); // cap the delta to [-1,1] for cross browser consistency
 
-    pos += -delta * speed
-    pos = Math.max(0, Math.min(pos, (target.scrollHeight - target.clientHeight) + (smooth * 2))) // limit scrolling
+    pos += -delta * speed;
+    pos = Math.max(
+      0,
+      Math.min(pos, target.scrollHeight - target.clientHeight + smooth * 2)
+    ); // limit scrolling
 
-    if (!moving) update()
+    if (!moving) update();
   }
 
   function update() {
-    moving = true
+    moving = true;
     let delta = (pos - target.scrollTop) / smooth;
 
     delta -= 1;
 
-    if(pos - target.scrollTop === smooth * 2)
-      delta = 0;
+    if (pos - target.scrollTop === smooth * 2) delta = 0;
 
-    target.scrollTop += delta
-    if (Math.abs(delta) > 0.5)
-      requestFrame(update)
-    else
-      moving = false
+    target.scrollTop += delta;
+    if (Math.abs(delta) > 0.5) requestFrame(update);
+    else moving = false;
   }
 
-  var requestFrame = function() { // requestAnimationFrame cross browser
+  var requestFrame = (function () {
+    // requestAnimationFrame cross browser
     return (
       window.requestAnimationFrame ||
       window.webkitRequestAnimationFrame ||
       window.mozRequestAnimationFrame ||
       window.oRequestAnimationFrame ||
       window.msRequestAnimationFrame ||
-      function(func) {
+      function (func) {
         window.setTimeout(func, 1000 / 50);
       }
     );
-  }()
-}
+  })();
+};
 
 export default SmoothScroll;
