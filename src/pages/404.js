7d2c467a6485a4448f3528b81c5163dce3997fb8
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2021-07-07 11:39:02 +0700

    update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,25 @@
 import * as React from "react"
-import { Helmet } from "react-helmet";
 
 // Css
 import * as styles from "../styles/modules/404.module.scss";
 
+// Layout
+import MainLayout from "../components/layout/mainLayout";
+import NavScroll from "../components/navScroll";
+
 // markup
 const NotFoundPage = () => {
   return (
-    <main id={styles.notfound}>
-      <Helmet>
-        <title>404 Not Found</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1,viewport-fit=cover"
-        />
-      </Helmet>
-      <div>
-        <p>
-          404 Not Found
-        </p>
+    <MainLayout pageTitle="404 Not Found">
+      <NavScroll topTitle="Back to Projects" topLink="projects" />
+      <div id={styles.notfound}>
+        <div>
+          <p>
+            404 Not Found
+          </p>
+        </div>
       </div>
-    </main>
+    </MainLayout>
   )
 }
 
